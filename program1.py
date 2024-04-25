b892c7b3017c4c5ba5f51f5861796b42bf61cf77
pandeyjaya <168068749+pandeyjaya@users.noreply.github.com>
2024-04-25 14:01:31 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,9 +1,18 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+       def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+
+    return not stack
         pass
     
 
